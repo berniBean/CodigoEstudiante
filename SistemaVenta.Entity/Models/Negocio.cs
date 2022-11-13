33fd6e1c3a114e3e159f53d69fbf9eb97963a54a
berniBean <ernesto_fope55@hotmail.com>
2022-11-12 22:57:30 -0600

    Cambios de nombresNegocios

--- SistemaVenta.Entity/Models/Negocio.cs
+++ SistemaVenta.Entity/Models/Negocio.cs
@@ -3,14 +3,14 @@
     public class Negocio
     {
         public Guid NegocioId { get; set; }
-        public string? UrlNegocio { get; set; }
+        public string? UrlLogo { get; set; }
         public string? NombreLogo { get; set; }
-        public string? NombreDocumento { get; set; }
+        public string? NumeroDocumento { get; set; }
         public string? Nombre { get; set; }
         public string? Correo { get; set; }
         public string? Direccion { get; set; }
         public string? Telefono { get; set; }
-        public decimal PorcentajeImpuestos { get; set; }
+        public decimal? PorcentajeImpuestos { get; set; }
         public string? SimboloMoneda { get; set; }
     }
 }
