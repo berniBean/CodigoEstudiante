4b83a9989412047ac5d406d8f647333432883171
berniBean <ernesto_fope55@hotmail.com>
2022-11-15 13:26:24 -0600

    Revert "Revert "Implementacion de productos""
    This reverts commit db0160f739d12cc48dec528e486cecb8e42f64fa.

---
+++ SistemaVentas.BLL/Interfaces/IProductoService.cs
@@ -0,0 +1,12 @@
+﻿using SistemaVenta.Entity.Models;
+
+namespace SistemaVenta.BLL.Interfaces
+{
+    public interface IProductoService
+    {
+        Task<List<Producto>> Lista();
+        Task<Producto> Crear(Producto entidad, Stream? Foto=null, string? NombreFoto=null);
+        Task<Producto> Editar(Producto entidad, Stream? Foto = null);
+        Task<bool> Eliminar(Guid idEntidad);
+    }
+}
